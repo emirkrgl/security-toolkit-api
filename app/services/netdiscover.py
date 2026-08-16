6eae86bd35c4a021e5783f1e74a02b9d9bcb8801
emirkrgl <koroglu.2616@gmail.com>
2026-08-16 19:08:01 +0300

    new

--- app/services/netdiscover.py
+++ app/services/netdiscover.py
@@ -9,15 +9,11 @@ def get_hostname(ip):
 def run(target_network, timeout=3):
     packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target_network)
     try:
-        #print(f" {target_network} Tarama başlatılıyor, lütfen bekleyin...\n")
         result = srp(packet, timeout=timeout, verbose=False, inter=0.1)[0]
-        #print("IP Adresi\t\tMAC Adresi\t\tCİHAZ ADI")
-        #print("------------------------------------------------------------------")
-
         cihaz_listesi = []
         for sent, received in result:
             name = get_hostname(received.psrc)
-            #print(f"{received.psrc:<20} {received.hwsrc}            {name}")
+
             cihaz_listesi.append({
                 "ip": received.psrc,
                 "mac": received.hwsrc,
