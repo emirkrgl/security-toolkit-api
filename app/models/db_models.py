4acb7975879119deff040a313cfc7d81f02c42ce
emirkrgl <koroglu.2616@gmail.com>
2026-08-22 21:03:37 +0300

    add database

---
+++ app/models/db_models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column,Integer,String,DateTime
+from app.core.database import Base
+from datetime import datetime,timezone
+class ScanResult(Base):
+    __tablename__="toolkit"
+    id=Column(Integer,primary_key=True,index=True)
+    task_id=Column(String,nullable=False)
+    tool=Column(String,nullable=False)
+    target=Column(String)
+    status=Column(String,nullable=False)
+    result=Column(String)
+    created_at=Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+
--- app/routers/portscanner.py
+++ app/routers/portscanner.py
@@ -1,24 +1,36 @@
-from fastapi import APIRouter,BackgroundTasks
+from fastapi import APIRouter,BackgroundTasks,Depends
 from app.services.portscanner import run
 from app.models.portscanner import PortScanRequest
 import uuid
+from app.core.database import get_db
+from app.models.db_models import ScanResult
+from app.core.database import SessionLocal
+from sqlalchemy.orm import Session
+import json
 router=APIRouter()
-tasks={}
 @router.get("/scan/status/{task_id}")
-def get_status(task_id:str):
-   if task_id in tasks:
-        return tasks[task_id] 
+
+def get_status(task_id:str,db:Session=Depends(get_db)):
+   a=db.query(ScanResult).filter(ScanResult.task_id==task_id).first()
+   if a:
+      return {"status":a.status,"result":json.loads(a.result) if a.result else None}
    else:
-        return {"error": "Bu task_id ile eşleşen bir işlem bulunamadı."}
+      return {"error":"Bu task_id ile eşleşen bir işlem bulunamadı."}
+   
 def run_and_store(task_id, target, start_port, end_port):
-   a=run(target, start_port, end_port)
-   tasks[task_id]={"status": "done", "result":a}
+   db=SessionLocal()
+   a=db.query(ScanResult).filter(ScanResult.task_id == task_id).first()
+   a.status="done"
+   a.result=json.dumps(run(target, start_port, end_port))
+   db.commit()
+   db.close()
+
 @router.post("/scan/port")
-def scan_port(request:PortScanRequest,background_task:BackgroundTasks):
+def scan_port(request:PortScanRequest,background_task:BackgroundTasks,db:Session=Depends(get_db)):
+   
    task_id=str(uuid.uuid4())
-   tasks[task_id]={
-      "status":"running",
-      "result":None
-   }
+   yeni_nesne=ScanResult(task_id=task_id, tool="port_scanner", target=request.target, status="running", result=None)
+   db.add(yeni_nesne)
+   db.commit()
    background_task.add_task(run_and_store,task_id,request.target,request.start_port,request.end_port)
    return {"task_id": task_id}
